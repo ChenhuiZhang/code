c26f8ce3f5d34dd5b65492ff5b91568e83741ca0
Hermes Zhang <chenhuiz@axis.com>
2017-05-15 19:59:33 +0800

    Replace the ssh api

--- web/mysite/mysite/view.py
+++ web/mysite/mysite/view.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import render
-from pexpect import pxssh
+import paramiko
 import os
 import re
 import ast
@@ -11,7 +11,7 @@ def search(request):
     for i, item in enumerate(item_list):
         print item.split();
         item_list[i] = [item.split()[0],                                                # ip
-                re.search('AXIS [AQPM][0-9]{4}(?:-[A-Z]{1,3})?[^-]*', item).group(),    # Product name
+                re.search('AXIS [AFMPQ][0-9]{2,4}(?:-[A-Z]{1,3})?[^-]*', item).group(),    # Product name
                 re.search('[0-9A-F]{12}', item).group()];                               # Mac
 
     return render_to_response('search.html', locals())
@@ -20,17 +20,18 @@ def device(request):
     if request.method == 'GET':
         print "Geeeeeeeeeeeeeeeeeet"
         print request.GET['ip']
-        s = pxssh.pxssh()
-        s.login(request.GET['ip'], 'root', 'pass')
-        s.sendline('bootblocktool -l')   # run a command
-        s.prompt()             # match the prompt
-        print(s.before)        # print everything before the prompt.
-        rsp = s.before.split('\r')
         bb_list = [];
-        for i, item in enumerate(rsp):
-            #print item.split('=');
-            #item = item.replace("\n", "");
-            item.strip()
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(request.GET['ip'], 22, "root", "pass")
+        stdin, stdout, stderr = ssh.exec_command("bootblocktool -l")
+        rsp_list = stdout.readlines()
+        ssh.close()
+
+        for i, item in enumerate(rsp_list):
+            print item.split('=');
+            item.strip();
             if '=' in item:
                 bb_list.append([item.split('=')[0], item.split('=')[1]]);
 
