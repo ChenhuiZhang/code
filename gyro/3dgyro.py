23014a147bf734fc118fec164f27964d23f89e71
Hermes Zhang <chenhuiz@axis.com>
2018-05-04 09:11:11 +0800

    Make it work

--- gyro/3dgyro.py
+++ gyro/3dgyro.py
@@ -1,4 +1,7 @@
 import pygame
+import requests
+import math
+from requests.auth import HTTPDigestAuth
 from pygame.locals import *
 
 from OpenGL.GL import *
@@ -11,8 +14,8 @@ verticies = [
     (-1, -0.1, -0.5),
     (1, -0.1, 0.5),
     (1, 0.1, 0.5),
-    (-1, -0.1, 0.5),
     (-1, 0.1, 0.5),
+    (-1, -0.1, 0.5),
 ]
 
 edges = [
@@ -21,13 +24,13 @@ edges = [
     (0, 4),
     (2, 1),
     (2, 3),
-    (2, 7),
-    (6, 4),
-    (6, 3),
-    (6, 7),
+    (2, 6),
+    (7, 4),
+    (7, 3),
+    (7, 6),
     (5, 1),
     (5, 4),
-    (5, 7),
+    (5, 6),
 ]
 
 surfaces = [
@@ -36,14 +39,61 @@ surfaces = [
     (6, 7, 5, 4),
     (4, 5, 1, 0),
     (1, 5, 7, 2),
-    (4, 0, 3, 6)
+    (4, 0, 3, 6),
 ]
 
-def Cube():
+triangles = [
+    (0, 1, 2),
+    (0, 3, 2),
+    (4, 5, 6),
+    (4, 7, 6),
+    (1, 2, 6),
+    (1, 5, 6),
+    (0, 3, 7),
+    (0, 4, 7),
+    (0, 1, 5),
+    (0, 4, 5),
+    (2, 3, 7),
+    (2, 6, 7),
+]
+
+def dist(a,b):
+	return math.sqrt((a*a)+(b*b))
+
+def get_y_rotation(x,y,z):
+	radians = math.atan2(x, dist(y,z))
+	return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+	radians = math.atan2(y, dist(x,z))
+	return math.degrees(radians)
+
+def read_values():
+    link = "http://192.168.77.183/axis-cgi/admin/uptime.cgi" # Change this address to your settings
+    r = requests.get(link, auth=HTTPDigestAuth('root', 'pass'))
+    values = r.text.split(' ')
+    accel_x = float(values[0])/16384.0
+    accel_y = float(values[1])/16384.0
+    accel_z = float(values[2])/16384.0
+
+    x_rot = get_x_rotation(accel_x, accel_y, accel_z);
+    y_rot = get_y_rotation(accel_x, accel_y, accel_z);
+
+    return str(x_rot) + " " + str(y_rot)
+
+def Cube_line():
     glBegin(GL_LINES)
     glColor3fv((0.2, 1, 1))
     for edge in edges:
         for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+def Cube_triangle():
+    glBegin(GL_TRIANGLES)
+    glColor3fv((0.0, 0.8, 0.8))
+    for triangle in triangles:
+        for vertex in triangle:
             glVertex3fv(verticies[vertex])
     glEnd()
 
@@ -111,15 +161,17 @@ def run():
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         background()
-        glPushMatrix()
-        glRotate(float(-28.032), 1, 0, 0)
-        glRotate(-float(-39.4132), 0, 0, 1)
+        values = read_values().split(' ')
 
-        Cube()
+        glPushMatrix()
+        glRotate(float(values[0]), 1, 0, 0)
+        glRotate(-float(values[1]), 0, 0, 1)
+
+        Cube_triangle()
         glPopMatrix()
 
         pygame.display.flip()
-        pygame.time.wait(10)
+        #pygame.time.wait(10)
 
 
 if __name__ == "__main__":
