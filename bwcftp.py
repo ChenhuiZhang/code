4dfaa96c36d37ad10ca3c23dd721c4a6e4107549
Hermes Zhang <chenhuiz@axis.com>
2019-06-03 20:05:01 +0800

    Code refactoring
    Change-Id: I979d5f60141417b3f44fbe4875a60021142dd335

--- bwcftp.py
+++ bwcftp.py
@@ -3,62 +3,59 @@
 from ftplib import FTP
 import re
 import os
+import sys
 
 mkvInfoList = []
 mkvLinkList = []
 
 def xxxx(dirinfo):
     if re.search("mkv$", dirinfo):
-        print(dirinfo)
         mkvInfoList.append(dirinfo)
 
+def records_fetch(ftp):
 
-def yyyy():
-    print("Retrive below record files from:")
-    for f in mkvInfoList:
-        print("1) ", f)
-    num = input("Choose the one you want to download: ")
-    print(num)
+    res = [x for x in ftp.nlst() if re.search("^[0-9]{8}$", x)]
+    ftp.cwd(res[0])
+    res = [x for x in ftp.nlst() if re.search("^[0-9]{2}$", x)]
+    res.sort(reverse=True)
+    ftp.cwd(res[0])
+    res = [x for x in ftp.nlst() if re.search("^[0-9]{8}_[0-9]{6}_.*", x)]
+    res.sort(reverse=True)
 
-ftp = FTP("fe80::aecc:8eff:fed2:497f%eth1")
-ftp.login(user="root", passwd="pass")
-ftp.cwd('/var/spool/storage/SD_DISK/current/recordings')
-ftp.retrlines('LIST')
-#print(ftp.dir())
-print(ftp.nlst())
+    for rec in res:
+        res = [x for x in ftp.nlst(rec) if re.search("^[0-9]{8}_[0-9]{2}$", x)]
+        ftp.dir(f'{rec}/{res[0]}', xxxx)
 
-res = [x for x in ftp.nlst() if re.search("^[0-9]{8}$", x)] 
-ftp.cwd(res[0])
-res = [x for x in ftp.nlst() if re.search("^[0-9]{2}$", x)] 
-res.sort(reverse=True)
-ftp.cwd(res[0])
-res = [x for x in ftp.nlst() if re.search("^[0-9]{8}_[0-9]{6}_.*", x)] 
-res.sort(reverse=True)
-print(res[0], res[1])
+        mkvs = [x for x in ftp.nlst(f'{rec}/{res[0]}') if re.search("mkv$", x)]
+        mkvLinkList.append(f'{rec}/{res[0]}/{mkvs[0]}')
 
-for rec in res:
-    res = [x for x in ftp.nlst(rec) if re.search("^[0-9]{8}_[0-9]{2}$", x)] 
-    ftp.dir(f'{rec}/{res[0]}', xxxx)
-
-    mkvs = [x for x in ftp.nlst(f'{rec}/{res[0]}') if re.search("mkv$", x)] 
-    mkvLinkList.append(f'{rec}/{res[0]}/{mkvs[0]}')
-
-print(mkvInfoList)
-print(mkvLinkList)
+    #print(mkvInfoList)
+    #print(mkvLinkList)
 
 
-print("Retrive below record files from:")
-for f in mkvInfoList:
-    print("1) ", f)
-val = input("Choose the one you want to download: ")
-print(val)
+def main():
+    ftp = FTP(sys.argv[1])
+    ftp.login(user="root", passwd="pass")
+    ftp.cwd('/var/spool/storage/SD_DISK/current/recordings')
 
-try:
-    num = int(val)
-except ValueError:
-    print("That's not an int!")
+    records_fetch(ftp)
 
-path = mkvLinkList[num-1]
+    print("Retrive below record files from:", sys.argv[1])
+    for line in mkvInfoList:
+        print(mkvInfoList.index(line), ") ", line)
+    val = input("Choose the one you want to download: ")
 
-print(path)
-ftp.retrbinary(f'RETR {path}', open(os.path.basename(path), 'wb').write)
+    try:
+        num = int(val)
+    except ValueError:
+        print("Input not valid!")
+
+    path = mkvLinkList[num]
+
+    ftp.retrbinary(f'RETR {path}', open(os.path.basename(path), 'wb').write)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
